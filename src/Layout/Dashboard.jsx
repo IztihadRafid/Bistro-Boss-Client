99f1a0c48ce295026889e537bddfecb7ff60bc4c
IztihadRafid <iztihadrafid10@gmail.com>
2025-01-05 21:35:23 +0600

    admin creation added in dashboard

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,53 +1,78 @@
-import { FaClipboardList, FaHome, FaShoppingCart } from "react-icons/fa";
+import { FaBook, FaClipboardList, FaHome, FaListUl, FaShoppingCart } from "react-icons/fa";
+import { ImSpoonKnife } from "react-icons/im";
 import { IoMdMenu } from "react-icons/io";
-import { MdEmail, MdOutlineRateReview, MdOutlineShoppingBag, MdOutlineWorkHistory } from "react-icons/md";
+import { MdEmail, MdGroups, MdOutlineRateReview, MdOutlineShoppingBag, MdOutlineWorkHistory } from "react-icons/md";
 import { SlCalender } from "react-icons/sl";
 import { NavLink, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
+    const isAdmin = true;
     return (
-       <div className="flex">
-        {/* side bar */}
-        <div className="w-64 min-h-screen bg-amber-400">
-            <ul className="menu">
-                <li>
-                    <NavLink to="/dashboard/userHome"><FaHome></FaHome> User Home</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/dashboard/reservation"><SlCalender />Reservation</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/dashboard/cart"><FaShoppingCart></FaShoppingCart>My Cart</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/dashboard/review"><MdOutlineRateReview />Add Review</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/dashboard/paymentHistory"><MdOutlineWorkHistory />Payment History</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/dashboard/bookings"><FaClipboardList />My Bookings</NavLink>
-                </li>
-                <div className="divider"></div>
-                <li>
-                    <NavLink to="/"><FaHome></FaHome>Home</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/order/salad"><IoMdMenu />Menu</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/"><MdOutlineShoppingBag />Shop</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/"><MdEmail />Contact</NavLink>
-                </li>
-            </ul>
+        <div className="flex">
+            {/* side bar */}
+            <div className="w-72 px-8 min-h-screen text-white bg-amber-800">
+                <div className="my-10">
+                <h1 className="uppercase text-3xl font-bold font-serif">Bistro Boss</h1>
+                <h3 className="uppercase text-xl font-semibold  font-serif">Restaurent</h3>
+                </div>
+                <ul className="menu text-lg">
+                    {
+                        isAdmin ? <> <li>
+                            <NavLink to="/dashboard/adminHome"><FaHome></FaHome> Admin Home</NavLink>
+                        </li>
+                            <li>
+                                <NavLink to="/dashboard/addItems"><ImSpoonKnife />Add Items</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/dashboard/manageItems"><FaListUl />Manage Items</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/dashboard/bookings"><FaBook />Manage Bookings</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/dashboard/users"><MdGroups />All Users</NavLink>
+                            </li>
+                           </> : //IF NOT ADMIN
+                            <> <li>
+                                <NavLink to="/dashboard/userHome"><FaHome></FaHome> User Home</NavLink>
+                            </li>
+                                <li>
+                                    <NavLink to="/dashboard/reservation"><SlCalender />Reservation</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/dashboard/cart"><FaShoppingCart></FaShoppingCart>My Cart</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/dashboard/review"><MdOutlineRateReview />Add Review</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/dashboard/paymentHistory"><MdOutlineWorkHistory />Payment History</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/dashboard/bookings"><FaClipboardList />My Bookings</NavLink>
+                                </li></>
+                    }
+                    {/* Shared Navlinks */}
+                    <div className="divider"></div>
+                    <li>
+                        <NavLink to="/"><FaHome></FaHome>Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/order/salad"><IoMdMenu />Menu</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/"><MdOutlineShoppingBag />Shop</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/"><MdEmail />Contact</NavLink>
+                    </li>
+                </ul>
+            </div>
+            {/* dashboard content */}
+            <div className="flex-1 p-8">
+                <Outlet></Outlet>
+            </div>
         </div>
-        {/* dashboard content */}
-        <div className="flex-1 p-8">
-            <Outlet></Outlet>
-        </div>
-       </div>
     );
 };
 
---
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -0,0 +1,101 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import { FaTrash, FaUser, FaUsers } from "react-icons/fa";
+import Swal from "sweetalert2";
+
+const AllUsers = () => {
+    const axiosSecure = useAxiosSecure()
+    const { data: users = [], refetch } = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
+            const res = await axiosSecure.get('/users');
+            return res.data
+        }
+    })
+
+    const handleMakeAdmin=(user)=>{
+        axiosSecure.patch(`/users/admin/${user._id}`)
+        .then(res=>{
+            console.log(res.data);
+            if(res.data.modifiedCount>0){
+                refetch();
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: `${user.name} is an Admin now`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  });
+            }
+
+        })
+    }
+
+
+    const handleDelete = user => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                axiosSecure.delete(`/users/${user._id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            refetch()
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "User Has Deleted",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+    }
+    return (
+        <div>
+            <div className="flex justify-evenly my-4">
+                <h2 className="text-3xl">All Users</h2>
+                <h2 className="text-3xl">Total Users :{users.length}</h2>
+            </div>
+            <div className="overflow-x-auto">
+                <table className="table table-zebra w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user, index) => <tr key={user._id}>
+                                <th>{index + 1}</th>``
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{
+                                        user.role ==='admin' ?"Admin" : <button onClick={() => handleMakeAdmin(user)} className="btn font-xl  "><FaUsers className="text-xl text-red-500"></FaUsers> </button>
+                                    }
+                                </td>
+                                <td> <button onClick={() => handleDelete(user)} className="btn btn-ghost btn-xs"><FaTrash className="text-xl text-red-500"></FaTrash> </button></td>
+                            </tr>)
+                        }
+
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default AllUsers;
